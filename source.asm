61000ea9b2dc52feb0f8fd7ee9f8af581dd65df4
Kevin Sem Piedra Matamoros <sempiedram@gmail.com>
2017-04-23 01:40:45 -0600

    Implement compare_strings method

--- source.asm
+++ source.asm
@@ -21,6 +21,11 @@
 	; This byte is either 0, or 1. 0 means that the program should stop, and 1 that it should continue. It's checked every cycle to see if the program should stop.
     running: db 0
 
+    ; Commands (that start with #) recognized:
+    CMD_EXIT: db "exit", 0
+    CMD_VARS: db "vars", 0
+    CMD_ABOUT: db "about", 0
+
 
 .CODE
     .STARTUP
@@ -52,5 +57,58 @@
 
 ; This method processes the user_input string.
 process_input:
+	push EDI
+	push ESI
+		mov EDI, user_input
+		mov ESI, CMD_EXIT
+		call compare_strings
+		
+		jne .strings_not_equal
+			PutCh 'e'
+			jmp .done
+		.strings_not_equal:
+			PutCh 'n'
+			jmp .done
+		
+		.done:
+			PutCh '-'
+	pop ESI
+	pop EDI
 	ret
 
+
+; Check if the string at EDI is equal to the string at ESI. The main string is ESI.
+compare_strings:
+	push ESI
+	push EDI
+	push EAX
+		; This is the byte comparison.
+		.cycle:
+			mov AL, byte [ESI]
+			cmp AL, byte [EDI]
+			jne .not_equal
+				; Characters were equal:
+				cmp AL, 0
+				
+				; If the bytes were 0, return true, else continue with the next cycle.
+				jne .next_cycle
+					; The end of the strings have been reached.
+					; Return true:
+					; Zero flag is already 1
+					jmp .done
+			.not_equal:
+				; Return false:
+				; Zero flag is already 0
+				jmp .done
+			
+			.next_cycle:
+				inc ESI
+				inc EDI
+				jmp .cycle
+			; End of .cycle
+		
+		.done:
+	pop EAX
+	pop EDI
+	pop ESI
+	ret
