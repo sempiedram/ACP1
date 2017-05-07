c2ba2bc1a78589c1ff89e75bdfd9edf2800c8ccd
Kevin Sem Piedra Matamoros <sempiedram@gmail.com>
2017-05-06 23:26:16 -0600

    Implement add_result_base_indicator

--- arithmetic_preprocessing.asm
+++ arithmetic_preprocessing.asm
@@ -4,7 +4,7 @@
 ; Adds base result indicator to expressions that don't have it.
 arithmetic_preprocessing:
 	call add_base_indicators
-	; call add_result_base_indicator
+	call add_result_base_indicator
 	ret
 
 
@@ -87,28 +87,131 @@ is_token_decimal_number:
 
 
 add_result_base_indicator:
-	; look for '=' token
-	; if there is one {
-		; if there is two '=' {
-			; raise error
-		; }else {
-			; if there are no tokens after '=' {
+	push EAX
+	push EBX
+	push ECX
+	push EDX
+	push ESI
+		; look for '=' token
+		mov AL, RESULT_BASE_INDICATOR_CHAR
+		mov EBX, user_input
+		call find_character_position
+		; Now '=''s position in user_input is in ECX, or ECX = -1 if it was not found.
+		
+		; if there is one {
+		mov EDX, ECX ; Save first '='s position.
+		cmp ECX, -1
+		je .no_base_indicator
+		
+			; if there is two '=' {
+			mov EBX, ECX ; Ignore the current '='
+			inc EBX
+			call find_character_position
+			
+			
+			cmp ECX, -1
+			je .not_two
+			
 				; raise error
+				mov byte [error_code], ERROR_INVALID_EXPRESSION
+				mov byte [error_reason], REASON_MULTIPLE_RESULT_BASE
+				
+				jmp .end
 			; }else {
-				; if more than one tokens after '=' {
+			.not_two:
+			
+				; if there are no tokens after '=' {
+				mov ESI, EDX ; ESI = position of '='
+				inc ESI ; Ignore the '='
+				call get_next_token ; Should be the base.
+				cmp byte [token_space], 0
+				jne .more_tokens
+				
 					; raise error
+					mov byte [error_code], ERROR_INVALID_EXPRESSION
+					mov byte [error_reason], REASON_NOT_ENOUGH_TOKENS
+					mov dword [error_extra_info2], token_space
+					
+					jmp .end
 				; }else {
-					; if token is valid base {
-						; end
-					; }else {
+				.more_tokens:
+				
+					; if more than one tokens after '=' {
+					call get_next_token
+					cmp byte [token_space], 0
+					je .no_more_tokens
+					
 						; raise error
+						mov byte [error_code], REASON_TOO_MANY_AFTER
+						mov dword [error_extra_info2], EDX
+						
+						jmp .end
+					; }else {
+					.no_more_tokens:
+					
+						; if token is valid base {
+						mov ESI, EDX ; ESI = position of '='
+						inc ESI ; Ignore the '='
+						call get_next_token ; Should be base.
+						
+						; Get base from token_space:
+						mov ESI, token_space
+						call get_base_from_string
+						; Now AL = 0 (invalid), 2, 8, 10, or 16
+						
+						cmp AL, 0 ; If not valid.
+						je .not_valid_base
+						
+							; end
+							; There is a valid base result indicator.
+							jmp .end
+							
+						; }else {
+						.not_valid_base:
+						
+							; raise error
+							PutCh '$'
+							PutCh '$'
+							mov byte [error_code], ERROR_INVALID_BASE
+							mov dword [error_extra_info2], token_space
+							
+							jmp .end
+						; }
+						
 					; }
+					
 				; }
+				
 			; }
+			
+		; }else {
+		.no_base_indicator:
+		
+			; add " = dec" to the expression
+			
+			push ESI
+			push EDI
+			
+				; This finds the end of user_input
+				mov ESI, user_input
+				call find_next_zero_esi
+				mov EDI, ESI
+				
+				; This appends default_base_result_indicator to user_input
+				mov ESI, default_base_result_indicator
+				call clone_string_into
+			
+			pop EDI
+			pop ESI
+			
 		; }
-	; }else {
-		; add " = dec" to the expression
-	; }
+		
+		.end:
+	pop ESI
+	pop EDX
+	pop ECX
+	pop EBX
+	pop EAX
 	ret
 
 
