95f02efe1da514a6f567ed792d6438908fb69f40
Kevin Sem Piedra Matamoros <sempiedram@gmail.com>
2017-05-08 14:08:34 -0600

    Add message for showing the final result of the evaluation process converted.

--- source.asm
+++ source.asm
@@ -169,6 +169,9 @@
 		
 		; To show the result of evaluation of the expression.
 		str_evaluation_result db "Result of evaluation: '", 0
+
+		; To show the final result of the evaluation:
+		str_final_result db "Final result: '", 0
 		
 		; For printing all variables.
 		str_defined_variables db "These are the defined variables:", 0
